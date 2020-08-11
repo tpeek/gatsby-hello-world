9af458bd6b7b50424597608c0a1a59bea19af971
Tyler Peek (Seattle) <tpeek@us.imshealth.com>
2020-08-11 11:52:49 -0700

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Header from "../components/Header"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header>About Gatsby</Header>
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
